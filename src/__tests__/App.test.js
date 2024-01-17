98ae30b5a13b665f3973da2b92894590bf4b46b7
carmelitatalty <94077977+carmelitatalty@users.noreply.github.com>
2024-01-17 11:45:10 -0800

    added intergration testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,23 +1,74 @@
-import { render } from '@testing-library/react';
-import App from '../App';
+/* eslint-disable testing-library/no-node-access */
+import { render, within } from "@testing-library/react";
+import App from "../App";
+import userEvent from '@testing-library/user-event';
+import { getEvents } from "../api";
 
-describe('<App /> component', () => {
-    
+describe("<App /> component", () => {
   let AppDOM;
   beforeEach(() => {
     AppDOM = render(<App />).container.firstChild;
-  })
-  
-    test('renders list of events', () => {
-        expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
-      });
+  });
 
-      test('render CitySearch', () => {
-        expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
-        });
+  test("renders list of events", () => {
+    expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
+  });
 
-        test('render NumberOfEvents', () => {
-          expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
-        });
-  
+  test("render CitySearch", () => {
+    expect(AppDOM.querySelector("#city-search")).toBeInTheDocument();
+  });
+
+  test("render NumberOfEvents", () => {
+    expect(AppDOM.querySelector("#number-of-events")).toBeInTheDocument();
+  });
+});
+
+describe("<App /> integration", () => {
+  test("renders a list of events matching the city selected by the user", async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const CitySearchDOM = AppDOM.querySelector("#city-search");
+    const CitySearchInput = within(CitySearchDOM).queryByRole("textbox");
+
+    await user.type(CitySearchInput, "Berlin");
+    const berlinSuggestionItem =
+      within(CitySearchDOM).queryByText("Berlin, Germany");
+    await user.click(berlinSuggestionItem);
+
+    const EventListDOM = AppDOM.querySelector("#event-list");
+    const allRenderedEventItems =
+      within(EventListDOM).queryAllByRole("listitem");
+
+    const allEvents = await getEvents();
+    const berlinEvents = allEvents.filter(
+      (event) => event.location === "Berlin, Germany"
+    );
+
+    expect(allRenderedEventItems.length).toBe(berlinEvents.length > 32 ? 32 : berlinEvents.length);
+    allRenderedEventItems.forEach((event) => {
+
+      expect(event.textContent).toContain("Berlin, Germany");
+    });
+  });
+
+  test("allow user to enter other values for number of events", async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const NumberOfEvents = AppDOM.querySelector("#number-of-events");
+    const EventListDOM = AppDOM.querySelector("#event-list");
+
+    await user.type(NumberOfEvents, "{backspace}{backspace}10");
+    const allRenderedEventItems =
+      within(EventListDOM).queryAllByRole("listitem");
+
+      expect(NumberOfEvents.value).toBe('10')
+
+       
+
+    expect(allRenderedEventItems.length).toBe(10);
+  });
 });
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,29 +1,34 @@
 import { useState } from "react";
 
-const Event = (event) => {
+const Event = ({event}) => {
   let eventStartDate = new Date();
-//   if (event?.start?.dateTime) {
-//     eventStartDate = new Date(event?.start?.dateTime);
-//   }
+  if (event?.start?.dateTime) {
+    eventStartDate = new Date(event.start.dateTime);
+  }
   const [showDetail, setShowDetail] = useState(false);
 
   const toggleShowDetail = () => {
     setShowDetail(!showDetail);
   };
   return (
-    <li>
-      <h3 title="eventSummary" className="eventSummary">{event.summary}</h3>
+    <li className="event">
+      <h3 title="eventSummary" className="eventSummary">{event?.summary}</h3>
       <div className="eventTime">
-        ${eventStartDate.toDateString()} (${eventStartDate.toTimeString()})
+        {eventStartDate.toDateString()} {eventStartDate.toTimeString()}
       </div>
       <div className="eventInfo">
-        <span className="eventInfoSummary">${event.summary}</span>
-        <span className="eventInfoLocation">${event.location}</span>
+        <span className="eventInfoSummary">{event?.summary}</span>
+        <span> | </span>
+        <span className="eventInfoLocation">{event?.location}</span>
       </div>
       {showDetail ? (
         <>
-          <div className="eventDetail" role="article"></div>
-          <button onClick={toggleShowDetail}>Hide Detail</button>
+          <div className="eventDetail" role="article">
+            <h4>Abount Event:</h4>
+            <div><a href={event.htmlLink}>See details on Google Calendar</a></div>
+            <div>{event.description}</div>
+          </div>
+          <button onClick={toggleShowDetail} className="details-btn">Hide Detail</button>
         </>
       ) : (
         <button onClick={toggleShowDetail}>Show Detail</button>
